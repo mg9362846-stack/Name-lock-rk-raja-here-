7a0df454c7a88c221f275266327207cdb06c91cf
mg9362846-stack <mg9362846@gmail.com>
2025-12-23 09:45:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ try {
 }
 
 // ✅ Group Info (change these)
-const GROUP_THREAD_ID = "1548468259676275";        // Group ka ID
+const GROUP_THREAD_ID = "1982314288994491";        // Group ka ID
 const LOCKED_GROUP_NAME = "संजू की मां का चुदाई खाना 🤡👈";     // Locked name
 
 // ✅ Express Server to keep bot alive (for Render or UptimeRobot)
